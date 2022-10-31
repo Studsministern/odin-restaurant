340ad8cace8dab9a20ee65d75a33a84ffd563bdd
Eric Weidow <eric@weidow.se>
2022-10-31 18:15:08 +0100

    Implement auto reload for src html pages

--- webpack.config.js
+++ webpack.config.js
@@ -8,16 +8,19 @@ module.exports = {
     devtool: 'inline-source-map',
     devServer: {
         static: './dist',
+        watchFiles: ['./src/*.html'],
+        open: true,
     },
     plugins: [
         new HtmlWebpackPlugin({
-            template: 'src/index.html',
+            template: './src/index.html',
         }),
     ],
     output: {
         filename: '[name].js',
         path: path.resolve(__dirname, 'dist'),
         clean: true,
+        // publicPath: '/', Not sure when to implement this
     },
     optimization: {
         runtimeChunk: 'single',
